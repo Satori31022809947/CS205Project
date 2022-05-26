69c3f74b4d7db72c23a5b6833a29556e1249a9e2
Satori <3102809947@qq.com>
2022-05-26 20:03:32 +0800

    add Martix.h Martix.cpp

--- Matrix.h
+++ Matrix.h
@@ -1,3 +1,13 @@
+/*
+ * @Author: Satori 3102809947@qq.com
+ * @Date: 2022-05-26 11:51:58
+ * @LastEditors: Satori 3102809947@qq.com
+ * @LastEditTime: 2022-05-26 20:02:24
+ * @FilePath: \CS205Project\Matrix.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#ifndef Martix_H_
+#define Martix_H_
 
 template <class T>
 
@@ -6,7 +16,22 @@ class martix {
         T** a;
 
     public:
-        martix(int row=0,int col=0); 
-                
+        /**
+         * @description: construct a martix
+         * @param {int} col 
+         * @param {int} row
+         * @return {*}
+         */
+        martix(int col=0,int row=0);
+
+
+        
+
+
+        
+        
+        
+        
 
 };
+#endif Martix_H_
